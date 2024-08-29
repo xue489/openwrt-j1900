2378b21c4f876bb9c0e089b1e31899a5e092a0ab
xue489 <131638058+xue489@users.noreply.github.com>
2024-08-29 19:16:36 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,3 +21,4 @@
 git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
 git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 git clone https://github.com/CCnut/feed-netkeeper.git package/netkeeper
+git clone https://github.com/vernesong/OpenClash.git package/openclash
